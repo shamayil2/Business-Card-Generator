62ddd54860cb9963aebe3fb39e8c6133aabede28
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-05-20 01:07:42 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css"
+
+const App = () => {
+
+
+  return(
+    <>
+    <div className="row">
+      <div className="container">
+     <div className="user-details">
+      <div><label htmlFor="name">Full Name:</label><br />
+      <input id="name" type="text" /><br /></div>
+      <div><label htmlFor="job">Job Title:</label><br />
+      <input id="job" type="text" /><br /></div>
+      <div><label htmlFor="bio">Write Your Bio :</label><br />
+      <input id="bio" type="text" /><br /></div>
+      <div><label htmlFor="name">Full Name:</label><br />
+      <input id="name" type="text" /><br /></div>
+      <div><label htmlFor="job">Job Title:</label><br />
+      <input id="job" type="text" /><br /></div>
+      <div><label htmlFor="bio">Write Your Bio :</label><br />
+      <input id="bio" type="text" /><br /></div>
+      
+      
+      
+      </div>
+      <div className="business-card">
+
+      </div>
+      </div>
+     
+    </div>
+    </>
+  )
+
+
+}
+
+export default App;
