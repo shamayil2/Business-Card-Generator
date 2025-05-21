f4597deb65493fd7380d07443328cf7937169ae0
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-05-21 12:51:02 +0530

    Added QR Code and Vcf File Download Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css"
+import Favicon from "react-favicon"
+import QRCode from "react-qr-code"
 import {useState} from "react"
 const App = () => {
   const [contactData,setContactData] = useState({})
@@ -7,6 +9,30 @@ const App = () => {
     const value = event.target.value;
     setContactData({...contactData,[id]:value})
   }
+
+   const vcfData = `
+  BEGIN:VCARD
+  VERSION:3.0
+  FN:${contactData.name}
+  TEL;TYPE=CELL:${contactData.number}
+  EMAIL:${contactData.email}
+  END:VCARD
+  `;
+
+   function downloadVcfFile(){
+
+    const blob = new Blob([vcfData],{ type: "text/vcard;charset=utf-8" })
+    const url = URL.createObjectURL(blob)
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `${contactData.name}.vcf`
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    URL.revokeObjectURL(url)
+   
+  }
+
   return(
     <>
     <div className="row">
@@ -26,9 +52,9 @@ const App = () => {
       <input onChange={(event)=>contactDataFn(event)} id="website" type="text" /><br /></div>
       <div >
         <h4>Your Social Links:</h4>
-        <label htmlFor="">LinkedIn: </label><input type="text" /><br />
-        <label htmlFor="">Github: </label><input type="text" /><br />
-        <label htmlFor="">Twitter: </label><input type="text" /><br />
+        <label htmlFor="linkedin">LinkedIn: </label><input onChange={(event)=>contactDataFn(event)} id="linkedin" type="text" /><br />
+        <label htmlFor="github">Github: </label><input onChange={(event)=>contactDataFn(event)} id="github" type="text" /><br />
+        <label htmlFor="twitter">Twitter: </label><input onChange={(event)=>contactDataFn(event)} id="twitter" type="text" /><br />
 
 
       </div>
@@ -41,13 +67,24 @@ const App = () => {
         <div className="card-container">
           <img src="https://placehold.co/150x150?text=Profile+Pic" alt="" /><br />
           <div className="details-container">
-            <p>{contactData.name}</p>
-            <p>{contactData.job}</p>
+            <div>
+            <h5>{contactData.name}</h5>
+            <h6>{contactData.job}</h6>
             <p>{contactData.bio}</p>
             <p>{contactData.number}</p>
             <p>{contactData.email}</p>
             <p>{contactData.website}</p>
+            <p><a target="_blank" href={contactData.linkedin}>Linkedin</a><a target="_blank" href={contactData.twitter}>Twitter</a><a target="_blank" href={contactData.github}>Github</a></p>
+            </div>
+            
           </div>
+        </div>
+        <div className="qr-div">
+          <h2>Scan To Open Website</h2>
+        <QRCode value={contactData.website? contactData.website:"null"} size={100}/>
+        </div>
+        <div className="copy-vcf">
+        <button onClick={()=>downloadVcfFile()}>Download The Contact</button>
         </div>
         
       </div>
