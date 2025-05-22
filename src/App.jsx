fc0e38f37ef186fdc9da318dbbf05317d42e18ca
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-05-22 22:57:49 +0530

    Final Dev Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import "./App.css"
 import Favicon from "react-favicon"
 import QRCode from "react-qr-code"
-import {useState} from "react"
+import {useState,useEffect} from "react"
 const App = () => {
-  const [contactData,setContactData] = useState({})
+  // const [contactData,setContactData] = useState(()=>{
+
+  //   const savedInfo = localStorage.getItem("userInfo")
+  //   return savedInfo? JSON.parse(userInfo):{}
+
+  // })
+  //  const [userInfo,setUserInfo] = useState(()=>{
+
+  //   const savedInfo = localStorage.getItem("userInfo")
+  //   return savedInfo? JSON.parse(savedInfo):{}
+
+  // })
+  // useEffect(()=>{
+  //   const data = localStorage.getItem("userInfo")
+  //   if(data){
+  //     setUserInfo(JSON.parse(data))
+  //     setContactData(JSON.parse(data))
+  //   }
+  // },[])
+
+  // useEffect(()=>{
+  //   localStorage.setItem("userData",JSON.stringify(contactData))
+  //   setUserInfo(contactData)
+  // },[contactData])
+  // console.log(userInfo)
+
+  const [contactData, setContactData] = useState(() => {
+    const savedInfo = localStorage.getItem("userInfo")
+    return savedInfo ? JSON.parse(savedInfo) : {}
+  })
+
+  useEffect(() => {
+    localStorage.setItem("userInfo", JSON.stringify(contactData))
+  }, [contactData])
+
   function contactDataFn(event){
     const id = event.target.id;
     const value = event.target.value;
@@ -35,9 +69,11 @@ const App = () => {
 
   return(
     <>
-    <div className="row">
-      <div className="container">
-     <div className="user-details">
+    <div className="container">
+  
+      <div className="row">
+     <div className="user-details col-md-6 text-center py-4">
+         <h2>Enter Your Details</h2>
       <div><label htmlFor="name">Full Name:</label><br />
       <input onChange={(event)=>contactDataFn(event)} id="name" type="text" /><br /></div>
       <div><label htmlFor="job">Job Title:</label><br />
@@ -62,12 +98,12 @@ const App = () => {
       
       
       </div>
-      <div className="business-card">
+      <div className="business-card col-md-6 py-4 text-center">
         <h2>Your Business Card</h2>
-        <div className="card-container">
-          <img src="https://placehold.co/150x150?text=Profile+Pic" alt="" /><br />
-          <div className="details-container">
-            <div>
+        <div className="card-container ">
+          <img src="https://placehold.co/100x100?text=Profile+Pic" alt="" /><br />
+          <div className="details-container row">
+            <div className="container">
             <h5>{contactData.name}</h5>
             <h6>{contactData.job}</h6>
             <p>{contactData.bio}</p>
